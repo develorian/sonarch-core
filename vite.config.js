27bf5a304504349edc48b67aa485891f09014deb
Dev3L0r14n <thedevelorian@gmail.com>
2026-02-28 00:17:38 -0600

    build(deploy): configurar infraestructura para despliegue en GitHub Pages.
    Detalles:
    
    - Integración de paquete gh-pages par automatización.
    - Configuración de base URL en vite.config.js.
    - Adición de 404.html nativo para soporte de enrutador SPA.
    - Peparación de entorno para demostración Alpha 1 en vivo.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
+    base: '/sonarch-core/',
     plugins: [wasm(), topLevelAwait()],
     server: { port: 8080 }
 });
